3a1aa8f9982abcc1ec49933b8db98bb90e90921c
Michael.C Ryan <spacetime.engineer@gmail.com>
2019-01-04 05:54:34 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from sympy import *
 from IPython.display import display
-from field_equations import Metric
+from field_equations import SpaceTime
 
 
 def main():
@@ -9,6 +9,8 @@ def main():
     
     a, b, c, d, s, k, t, x, y, z, x0, x1, x2, x3, dt, dx, dy, dz, dx0, dx1, dx2, dx3, vx, vy, vz, vtau, dtau, dX, r, th, ph, G, M, r, i, k, l, R, v, f, w  = symbols('a b c d s k t x y z x0 x1 x2 x3 dt dx dy dz dx0 dx1 dx2 dx3 vx vy vz vtau dtau dX, r th ph G M r i k l R v f w')
 
+    
+    
     # The classic black hole solution. Uncharged and rotationally stationary.
     # PASSES!
     schwarzschild_spacetime = Matrix([    
@@ -17,7 +19,7 @@ def main():
                                           [ 0, 0, - x1**2, 0 ], 
                                           [ 0, 0, 0, - x1**2*sin(x2)**2 ]
                                      ])
-    
+    """
     # Flat spacetime expressed in hyperspherical coordinates.
     # PASSES!
     milne_spacetime = Matrix([
@@ -25,34 +27,52 @@ def main():
                                   [ 0, -x0**2, 0, 0 ], 
                                   [ 0, 0, -x0**2*sinh(x1)**2, 0 ],
                                   [ 0, 0, 0, -x0**2*sinh(x1)**2*sin(x2)**2 ]
+                             ])
     
     # Friedmann Lemaitre Robertson Walker solution.
     a, k = symbols('a k')
+    # PASSES!
     friedmann_lemaitre_robertson_walker_solution = Matrix([
-                                  [ 1, 0, 0, 0 ], 
-                                  [ 0, - a**2*(1-k*x1**2)**(-1), 0, 0 ], 
-                                  [ 0, 0, - a**2*x1**2, 0 ], 
-                                  [ 0, 0, 0, - a**2*x1**2*sin(x2)**2 ]
-                              ])
+                                                              [ 1, 0, 0, 0 ], 
+                                                              [ 0, - a**2*(1-k*x1**2)**(-1), 0, 0 ], 
+                                                              [ 0, 0, - a**2*x1**2, 0 ], 
+                                                              [ 0, 0, 0, - a**2*x1**2*sin(x2)**2 ]
+                                                          ])
     
     # Most famous wormhole solution.
     m = symbols('m')
     einstein_rosen_bridge = Matrix([
-                                  [ (x1-2*m) / x1, 0, 0, 0 ], 
-                                  [ 0, - 4 * x1 / ( 2 * x1 - 4 * m ), 0, 0 ], 
-                                  [ 0, 0, - x1**2, 0 ], 
-                                  [ 0, 0, 0, - x1**2 * sin(x2)**2 ]
-                              ])
+                                       [ (x1-2*m) / x1, 0, 0, 0 ], 
+                                       [ 0, - 4 * x1 / ( 2 * x1 - 4 * m ), 0, 0 ], 
+                                       [ 0, 0, - x1**2, 0 ], 
+                                       [ 0, 0, 0, - x1**2 * sin(x2)**2 ]
+                                   ])
+    
+    # Rotataing uncharged black hole.
+    a, J, M, c, delt, sigm = symbols('a J M c Delta Sigma')
+    a = (J/(M*c))
+    rs = (2*G*M/(c**2))
+    sigm = (x1**2 + (J/(M*c))**2 * cos(x2)**2)
+    delt = (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2)
+    kerr_spacetime = Matrix([
+                                [ (1 - rs * x1 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0, (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ], 
+                                [ 0, -1 * ( (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2) / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0 ], 
+                                [ 0, 0, -1 * (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0 ],
+                                [ (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0, 0, -1 * (x1**2 + (J/(M*c))**2 + (rs*x1*(J/(M*c))**2/(x1**2 + (J/(M*c))**2 * cos(x2)**2))*sin(x2))*sin(x2) ]
+                            ])
+    
+    
     
     # Original wormhole solution.
+    #PASSES
     ellis_drainhole_spacetime = Matrix([
-                                  [ 1, 0, 0, 0 ], 
-                                  [ 0, - 1, 0, 0 ], 
-                                  [ 0, 0, - ( k**2 + x1 ** 2 ), 0 ], 
-                                  [ 0, 0, 0, - (k**2 + x1 ** 2) * sin(x2)**2 ]
-                              ])
+                                           [ 1, 0, 0, 0 ], 
+                                           [ 0, - 1, 0, 0 ], 
+                                           [ 0, 0, - ( k**2 + x1 ** 2 ), 0 ], 
+                                           [ 0, 0, 0, - (k**2 + x1 ** 2) * sin(x2)**2 ]
+                                       ])
     
-    
+    #PASSES
     hypersphere_metric = Matrix([
                                     [ 1, 0, 0, 0 ],
                                     [ 0, -x0**2, 0, 0 ],
@@ -85,10 +105,10 @@ def main():
     sigm = x1**2 + a**2 * cos(x2)**2
     delt = (r**2 - x0 * rs + a**2)
     kerr_spacetime = Matrix([
-                              [ (1 - rs * x1 / sigm ), 0, 0, 2 * G * M / ( c**2 ) ], 
-                              [ 0, -1 * ( delt / sigm ), 0, 0 ], 
-                              [ 0, 0, -1 * sigm, 0 ],
-                              [ 2 * G * M / ( c**2 ), 0, 0, -1 * (x1**2 + a**2 + (rs*x1*a**2/sigm)*sin(x2))*sin(x2) ]
+                                [ (1 - rs * x1 / sigm ), 0, 0, 2 * G * M / ( c**2 ) ], 
+                                [ 0, -1 * ( delt / sigm ), 0, 0 ], 
+                                [ 0, 0, -1 * sigm, 0 ],
+                                [ 2 * G * M / ( c**2 ), 0, 0, -1 * (x1**2 + a**2 + (rs*x1*a**2/sigm)*sin(x2))*sin(x2) ]
                             ])
 
     #Ref: https://en.wikipedia.org/wiki/Taub%E2%80%93NUT_space
@@ -128,7 +148,8 @@ def main():
                                        [ 0, 0, -x1**2, 0 ], 
                                        [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
                                    ])
-    """
+    
+    
     # The classic warp drive solution. (This takes a long time to process!!!) (I suspect the compute time diverges. I think there is some recursive error)
     # Does not crash.
     xs = symbols('x_s')(x0)
@@ -136,18 +157,34 @@ def main():
     rs = sqrt((x1-xs)**2 + x2**2 + x3**2)
     fs = tanh(s * (rs + R)) - tanh(s * (rs - R)) / (2 * tanh( s * R ))
     alcubierre_spacetime = Matrix([
-                                   [ (vs**2 * fs**2 - 1), -2*vs*fs, -2*vs*fs, -2*vs*fs ], 
-                                   [ -2*vs*fs, -1, 0, 0 ], 
-                                   [ -2*vs*fs, 0, -1, 0 ], 
-                                   [ -2*vs*fs, 0, 0, -1 ]
-                               ])
+                                      [ (vs**2 * fs**2 - 1), -2*vs*fs, -2*vs*fs, -2*vs*fs ], 
+                                      [ -2*vs*fs, -1, 0, 0 ], 
+                                      [ -2*vs*fs, 0, -1, 0 ], 
+                                      [ -2*vs*fs, 0, 0, -1 ]
+                                  ])
+    
+    
+    # Rotataing uncharged black hole.
+    a, J, M, c, delt, sigm = symbols('a J M c Delta Sigma')
+    a = (J/(M*c))
+    rs = (2*G*M/(c**2))
+    sigm = (x1**2 + (J/(M*c))**2 * cos(x2)**2)
+    delt = (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2)
+    kerr_spacetime = Matrix([
+                                [ (1 - rs * x1 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0, (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ], 
+                                [ 0, -1 * ( (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2) / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0 ], 
+                                [ 0, 0, -1 * (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0 ],
+                                [ (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0, 0, -1 * (x1**2 + (J/(M*c))**2 + (rs*x1*(J/(M*c))**2/(x1**2 + (J/(M*c))**2 * cos(x2)**2))*sin(x2))*sin(x2) ]
+                            ])
+    
+    
     """
-
-
+    
     coordinate_set = [ x0, x1, x2, x3 ]
-    spacetime = Metric(kerr_newman_spacetime, coordinate_set)
+    spacetime = SpaceTime(schwarzschild_spacetime, coordinate_set)
 
-
+    
+    
     print("")
     print("")
     print("Metric tensor coefficients (dd)")
@@ -208,3 +245,10 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
