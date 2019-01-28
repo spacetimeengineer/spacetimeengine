e9469fa2943ff5459b264ab1ea72e52d561c1b35
Michael.C Ryan <spacetime.engineer@gmail.com>
2019-01-28 12:26:47 -0500

    Add files via upload

---
+++ solutions.py
@@ -0,0 +1,524 @@
+#!/usr/bin/env python
+from sympy import *
+
+class Solution:
+    
+    def minkowski(self, coordinates):
+        """
+        Description
+        ===========
+        Returns the Friedmann Lemaitre Robertson Walker metric which describes the spacetime for an expanding universe.
+        Examples
+        ========
+        >>> print(Solution().minkowski())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        # Friedmann Lemaitre Robertson Walker solution.
+        # PASSES!
+        minkowski_metric = Matrix([
+                                      [ 1, 0, 0, 0 ], 
+                                      [ 0, -1, 0, 0 ], 
+                                      [ 0, 0, -1, 0 ], 
+                                      [ 0, 0, 0, -1 ]
+                                  ])
+        
+        if (coordinates == "spherical"):
+            pass
+        elif (coordinates == "euclidan"):
+            minkowski_metric = Matrix([
+                                          [ 1, 0, 0, 0 ], 
+                                          [ 0, -1, 0, 0 ], 
+                                          [ 0, 0, -1, 0 ], 
+                                          [ 0, 0, 0, -1 ]
+                                      ])
+        else:
+            pass
+            
+        
+        return minkowski_metric
+    
+    def schwarzschild(self):    
+        """
+        Description
+        ===========
+        Returns the classic black hole solution. Uncharged and rotationally stationary.
+        Examples
+        ========
+        >>> print(Solution().schwarzschild())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        schwarzschild_metric = Matrix([    
+                                              [ (1-(2*G*M)/(x1*c**2)), 0, 0, 0 ], 
+                                              [ 0, - (1-(2*G*M)/(x1*c**2))**(-1), 0, 0 ], 
+                                              [ 0, 0, - x1**2, 0 ], 
+                                              [ 0, 0, 0, - x1**2*sin(x2)**2 ]
+                                         ])
+        return schwarzschild_metric
+    
+    def friedmann_lemaitre_robertson_walker(self):
+        """
+        Description
+        ===========
+        Returns the Friedmann Lemaitre Robertson Walker metric which describes the spacetime for an expanding universe.
+        Examples
+        ========
+        >>> print(Solution().friedmann_lemaitre_robertson_walker())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        # Friedmann Lemaitre Robertson Walker solution.
+        a, k = symbols('a k')
+        # PASSES!
+        x0, x1, x2, x3 = symbols('x0 x1 x2 x3')
+        coordinate_set = [ x0, x1, x2, x3 ]
+        friedmann_lemaitre_robertson_walker_metric = Matrix([
+                                                                  [ 1, 0, 0, 0 ], 
+                                                                  [ 0, - a**2*(1-k*x1**2)**(-1), 0, 0 ], 
+                                                                  [ 0, 0, - a**2*x1**2, 0 ], 
+                                                                  [ 0, 0, 0, - a**2*x1**2*sin(x2)**2 ]
+                                                              ])
+        return friedmann_lemaitre_robertson_walker_metric
+
+    # Most famous wormhole solution.
+    def einstein_rosen_bridge(self):
+        """
+        Description
+        ===========
+        Returns the metric for the most famous wormhole solution.
+        Examples
+        ========
+        >>> print(Solution().einstein_rosen_bridge())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        m = symbols('m')
+        einstein_rosen_bridge_metric = Matrix([
+                                           [ (x1-2*m) / x1, 0, 0, 0 ], 
+                                           [ 0, - 4 * x1 / ( 2 * x1 - 4 * m ), 0, 0 ], 
+                                           [ 0, 0, - x1**2, 0 ], 
+                                           [ 0, 0, 0, - x1**2 * sin(x2)**2 ]
+                                       ])
+        return einstein_rosen_bridge_metric
+    
+    def taub_nut(self):
+        """
+        Description
+        ===========
+        Returns Taub-Nut metric.
+        Examples
+        ========
+        >>> print(taub_nut())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        U, m, l = symbols('U m l')
+        U = (2 * m * x0 + l**2 - x0**2) / ( x0**2 + l**2 )
+        taub_nut_metric = Matrix([
+                                        [ U**(-1), 0, 0, 0 ], 
+                                        [ 0, -4 * l**2 * U, -1 * cos(x2) * (4 * l**2 * U), 0 ], 
+                                        [ 0, -1 * cos(x2) * (4 * l**2 * U), -1 * (x0**2 + l**2), 0 ],
+                                        [ 0, 0, 0, -1 * (x0**2 + l**2) * sin(x2) - (4 * l**2 * U) * cos(x2)**2 ]
+                                    ])
+        
+        return taub_nut_metric
+
+    def milne(self):
+        """
+        Description
+        ===========
+        Returns Milne metric.
+        Examples
+        ========
+        >>> print(Solution().milne())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        milne_metric = Matrix([
+                                  [ 1, 0, 0, 0 ], 
+                                  [ 0, -x0**2, 0, 0 ], 
+                                  [ 0, 0, -x0**2*sinh(x1)**2, 0 ],
+                                  [ 0, 0, 0, -x0**2*sinh(x1)**2*sin(x2)**2 ]
+                             ])
+        
+        
+        return milne_metric
+
+    def kerr(self):
+        """
+        Description
+        ===========
+        Returns Kerr metric.
+        Examples
+        ========
+        >>> print(Solution().kerr())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        x0, x1, x2, x3 = symbols('x0 x1 x2 x3')
+        a, J, M, c, G, r, delt, sigm = symbols('a J M c G r Delta Sigma')
+        a = (J/(M*c))
+        rs = (2*G*M/(c**2))
+        sigm = (x1**2 + (J/(M*c))**2 * cos(x2)**2)
+        delt = (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2)
+        kerr_metric = Matrix([
+                                    [ (1 - rs * x1 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0, (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ], 
+                                    [ 0, -1 * ( (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2) / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0 ], 
+                                    [ 0, 0, -1 * (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0 ],
+                                    [ (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0, 0, -1 * (x1**2 + (J/(M*c))**2 + (rs*x1*(J/(M*c))**2/(x1**2 + (J/(M*c))**2 * cos(x2)**2))*sin(x2))*sin(x2) ]
+                                ])
+
+        return kerr_metric
+
+    def alcubierre(self):
+        """
+        Description
+        ===========
+        Returns the famous Alcubierre 'warp-drive' metric solution.
+        Examples
+        ========
+        >>> from sympy import *
+        >>> print(Solution().alcubierre())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        # The classic warp drive solution. (This takes a long time to process!!!) (I suspect the compute time diverges. I think there is some recursive error)
+        # Does not crash.
+        xs = symbols('x_s')(x0)
+        vs = xs.diff(x0)
+        rs = sqrt((x1-xs)**2 + x2**2 + x3**2)
+        fs = tanh(s * (rs + R)) - tanh(s * (rs - R)) / (2 * tanh( s * R ))
+        alcubierre_spacetime = Matrix([
+                                          [ (vs**2 * fs**2 - 1), -2*vs*fs, -2*vs*fs, -2*vs*fs ], 
+                                          [ -2*vs*fs, -1, 0, 0 ], 
+                                          [ -2*vs*fs, 0, -1, 0 ], 
+                                          [ -2*vs*fs, 0, 0, -1 ]
+                                      ])    
+    
+    def ellis(self):
+        """
+        Description
+        ===========
+        Returns the original wormhole solution.
+        Examples
+        ========
+        >>> print(Solution().ellis())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+
+        ellis_drainhole_metric = Matrix([
+                                               [ 1, 0, 0, 0 ], 
+                                               [ 0, - 1, 0, 0 ], 
+                                               [ 0, 0, - ( k**2 + x1 ** 2 ), 0 ], 
+                                               [ 0, 0, 0, - (k**2 + x1 ** 2) * sin(x2)**2 ]
+                                           ])
+        return ellis_drainhole_metric
+    
+    def kerns_wild(self):
+        """
+        Description
+        ===========
+        Returns Kerns-Wild metric.
+        Examples
+        ========
+        >>> print(Solution().kerns_wild())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return kerns_wild_metric
+
+    def double_kerr(self):
+        """
+        Description
+        ===========
+        Returns Double-Kerr metric.
+        Examples
+        ========
+        >>> print(Solution().double_kerr())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return double_kerr_metric
+
+    def khan_penrose(self):
+        """
+        Description
+        ===========
+        Returns Khan metric.
+        Examples
+        ========
+        >>> print(Solution().khan_penrose())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return khan_metric
+
+    def gowdy(self):
+        """
+        Description
+        ===========
+        Returns Gowdy metric.
+        Examples
+        ========
+        >>> print(Solution().gowdy())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return gowdy_metric
+
+    def ehlers(self):
+        """
+        Description
+        ===========
+        Returns Ehlers metric.
+        Examples
+        ========
+        >>> print(Solution().ehlers())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return ehlers_metric
+
+    def weyl(self):
+        """
+        Description
+        ===========
+        Returns Weyl metric.
+        Examples
+        ========
+        >>> print(Solution().weyl())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return weyl_metric
+
+    def beck(self):
+        """
+        Description
+        ===========
+        Returns Beck metric.
+        Examples
+        ========
+        >>> print(Solution().beck())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return beck_metric
+
+    def ernst(self):
+        """
+        Description
+        ===========
+        Returns Ernst metric.
+        Examples
+        ========
+        >>> print(Solution().ernst())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return ernst_metric
+
+    def kasner(self):
+        """
+        Description
+        ===========
+        Returns Kanser metric.
+        Examples
+        ========
+        >>> print(Solution().kasner())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return kasner_metric
+
+    def kruskal_szekeres(self):
+        """
+        Description
+        ===========
+        Returns Kruskal Szekeres metric.
+        Examples
+        ========
+        >>> print(Solution().kruskal_szekeres())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return kruskal_szekeres_metric
+
+
+    #Electrovacum solutions.
+
+    def reissner_nordstrom(self):
+        """
+        Description
+        ===========
+        Returns Reissner Nordstrom metric.
+        Examples
+        ========
+        >>> print(Solution().reissner_nordstrom())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        G, M, c, Q, k = symbols('G M c Q k')
+        rs = 2 * G * M / ( c**2 )
+        rq = Q**2 * G * k / c**4
+        reissner_nordstrom_spacetime = Matrix([
+                                                  [ (1 + rs / x1 + rq**2 / x0**2), 0, 0, 0 ], 
+                                                  [ 0, -1 * (1 + rs / x1 + rq**2 / x0**2)**(-1), 0, 0 ], 
+                                                  [ 0, 0, -x1**2, 0 ], 
+                                                  [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
+                                              ])
+        
+        return kruskal_szekeres_metric
+
+    def kerr_newman(self):
+        """
+        Description
+        ===========
+        Returns Kerr-Newman metric.
+        Examples
+        ========
+        >>> print(Solution().kerr_newman())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        
+        x0, x1, x2, x3 = symbols('t r psi theta')
+        G, M, c, Q, k = symbols('G M c Q k')
+        rs = 2 * G * M / ( c**2 )
+        rq = Q**2 * G * k / c**4
+        kerr_newman_metric = Matrix([
+                                           [ (1 + rs / x1 + rq**2 / x0**2), 0, 0, 0 ], 
+                                           [ 0, -1 * (1 + rs / x1 + rq**2 / x0**2)**(-1), 0, 0 ], 
+                                           [ 0, 0, -x1**2, 0 ], 
+                                           [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
+                                       ])
+        
+        return kerr_newman_metric
+
+    def melvin(self):
+        """
+        Description
+        ===========
+        Returns Melvin metric.
+        Examples
+        ========
+        >>> print(Solution().melvin())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return melvin_metric
+
+    def garfinkle_melvin(self):
+        """
+        Description
+        ===========
+        Returns Garfinkle Melvin metric.
+        Examples
+        ========
+        >>> print(Solution().garfinkle_melvin())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return garfinkle_melvin_metric
+
+    def bertotti_robinson(self):
+        """
+        Description
+        ===========
+        Returns Bertotti Robinson metric.
+        Examples
+        ========
+        >>> print(Solution().bertotti_robinson())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return bertotti_robinson_metric
+
+    def vaidya(self):
+        """
+        Description
+        ===========
+        Returns Vaidya metric.
+        Examples
+        ========
+        >>> print(Solution().vaidya())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        return vaidya_metric
+
+    def ozsvath_schucking(self):
+        """
+        Description
+        ===========
+        Returns the ozsvath schucking metric.
+        Examples
+        ========
+        >>> print(Solution().ozsvath_schucking())
+        >>> 
+        LaTeX representation
+        ====================
+        """
+        ozsvath_schucking_metric = Matrix([
+                                                 [ 1, 0, -2*x3, 0 ], 
+                                                 [ 0, 0, 1*x3, 0 ], 
+                                                 [ -2*x3, 1*x3, -1, 1 ], 
+                                                 [ 0, 0, 1, 1 ]
+                                             ])
+        return ozsvath_schucking_metric
+    
+    def godel(self):
+        """
+        Description
+        ===========
+        Returns Godel metric. A famous classic spacetime which describes time travel.
+        Examples
+        ========
+        >>> print(Solution().godel())
+        >>> 
+        LaTeX representation
+        ====================
+        ds^{2}={\frac {1}{2\omega ^{2}}}\left[-(dt+e^{x}dy)^{2}+dx^{2}+{\tfrac {1}{2}}e^{2x}dy^{2}+dz^{2}\right],\qquad -\infty <t,x,y,z<\infty ,} {\displaystyle ds^{2}={\frac {1}{2\omega ^{2}}}\left[-(dt+e^{x}dy)^{2}+dx^{2}+{\tfrac {1}{2}}e^{2x}dy^{2}+dz^{2}\right],\qquad -\infty <t,x,y,z<\infty
+        """
+        
+        f = Rational('1/2') * w**(-2)
+        godel_metric = Matrix([
+                                      [ f, 0, f * 2*E**(2 * x1), 0 ], 
+                                      [ 0, -1, 0, 0 ], 
+                                      [ f * 2*E**(2 * x1), 0, -f*Rational('1/2')*E**x1, 0 ], 
+                                      [ 0, 0, 0, -f ]
+                                  ])
+        
+        return godel_metric
