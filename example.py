df58e6637b47417fb4221e0e5fb0a2e937155e0f
Michael.C Ryan <spacetime.engineer@gmail.com>
2019-01-31 17:12:52 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,302 +1,33 @@
 #!/usr/bin/env python
-from sympy import *
-#from IPython.display import display
-from spacetime import SpaceTime
+from solutions import *
+from spacetime import *
 
 
 def main():
-    init_printing()
     
-    a, b, c, d, s, k, t, x, y, z, x0, x1, x2, x3, dt, dx, dy, dz, dx0, dx1, dx2, dx3, vx, vy, vz, vtau, dtau, dX, r, th, ph, G, M, r, i, k, l, R, v, f, w  = symbols('a b c d s k t x y z x0 x1 x2 x3 dt dx dy dz dx0 dx1 dx2 dx3 vx vy vz vtau dtau dX, r th ph G M r i k l R v f w')
-
+    # Retrieves a known solution from the Solutions() class.
+    blackhole = Solution().schwarzschild()
     
+    # The current general model for describing the accellerated expansion of the universe.
+    #expanding_universe = Solution().friedmann_lemaitre_robertson_walker()
     
-    # The classic black hole solution. Uncharged and rotationally stationary.
-    # PASSES!
-    schwarzschild_spacetime = Matrix([    
-                                          [ (1-(2*G*M)/(x1*c**2)), 0, 0, 0 ], 
-                                          [ 0, - (1-(2*G*M)/(x1*c**2))**(-1), 0, 0 ], 
-                                          [ 0, 0, - x1**2, 0 ], 
-                                          [ 0, 0, 0, - x1**2*sin(x2)**2 ]
-                                     ])
-    """
-    # Flat spacetime expressed in hyperspherical coordinates.
-    # PASSES!
-    milne_spacetime = Matrix([
-                                  [ 1, 0, 0, 0 ], 
-                                  [ 0, -x0**2, 0, 0 ], 
-                                  [ 0, 0, -x0**2*sinh(x1)**2, 0 ],
-                                  [ 0, 0, 0, -x0**2*sinh(x1)**2*sin(x2)**2 ]
-                             ])
+    # The most famous wormhole solution.
+    #wormhole = Solution().einstein_rosen_bridge()
     
-    # Friedmann Lemaitre Robertson Walker solution.
-    a, k = symbols('a k')
-    # PASSES!
-    friedmann_lemaitre_robertson_walker_solution = Matrix([
-                                                              [ 1, 0, 0, 0 ], 
-                                                              [ 0, - a**2*(1-k*x1**2)**(-1), 0, 0 ], 
-                                                              [ 0, 0, - a**2*x1**2, 0 ], 
-                                                              [ 0, 0, 0, - a**2*x1**2*sin(x2)**2 ]
-                                                          ])
+    # Empty vacuum.
+    #flat_spacetime = Solution().minkowski()
     
-    # Most famous wormhole solution.
-    m = symbols('m')
-    einstein_rosen_bridge = Matrix([
-                                       [ (x1-2*m) / x1, 0, 0, 0 ], 
-                                       [ 0, - 4 * x1 / ( 2 * x1 - 4 * m ), 0, 0 ], 
-                                       [ 0, 0, - x1**2, 0 ], 
-                                       [ 0, 0, 0, - x1**2 * sin(x2)**2 ]
-                                   ])
+    # A special case of a more general form of the Friedmann Lemaitre Robertson Walker solution.
+    #empty_expanding_universe = Solution().milne()
     
-    # Rotataing uncharged black hole.
-    a, J, M, c, delt, sigm = symbols('a J M c Delta Sigma')
-    a = (J/(M*c))
-    rs = (2*G*M/(c**2))
-    sigm = (x1**2 + (J/(M*c))**2 * cos(x2)**2)
-    delt = (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2)
-    kerr_spacetime = Matrix([
-                                [ (1 - rs * x1 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0, (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ], 
-                                [ 0, -1 * ( (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2) / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0 ], 
-                                [ 0, 0, -1 * (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0 ],
-                                [ (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0, 0, -1 * (x1**2 + (J/(M*c))**2 + (rs*x1*(J/(M*c))**2/(x1**2 + (J/(M*c))**2 * cos(x2)**2))*sin(x2))*sin(x2) ]
-                            ])
+    # Gravity as we know it, without the high order effects.
+    #newtonian_gravity = Solution().weak_field_approximation()
     
     
+    # Creates the spacetime object which holds information of tensor coefficients. (A 'True' will suppress printing)
+    spacetime = SpaceTime(blackhole, False)
     
-    # Original wormhole solution.
-    #PASSES
-    ellis_drainhole_spacetime = Matrix([
-                                           [ 1, 0, 0, 0 ], 
-                                           [ 0, - 1, 0, 0 ], 
-                                           [ 0, 0, - ( k**2 + x1 ** 2 ), 0 ], 
-                                           [ 0, 0, 0, - (k**2 + x1 ** 2) * sin(x2)**2 ]
-                                       ])
-    
-    #PASSES
-    # I have studied this solution extensivly. Very interesting. Describes how a hypersphere radius is equivilent to cosmic time.
-    hypersphere_metric = Matrix([
-                                    [ 1, 0, 0, 0 ],
-                                    [ 0, -x0**2, 0, 0 ],
-                                    [ 0, 0, -x0**2*sin(x1)**2, 0 ],
-                                    [ 0, 0, 0, -x0**2*sin(x1)**2*sin(x2)**2 ]
-                                ])
-    
-    # Flat spacetime.
-    minkowski_metric = Matrix([
-                                  [ 1, 0, 0, 0 ], 
-                                  [ 0, -1, 0, 0 ], 
-                                  [ 0, 0, -1, 0 ], 
-                                  [ 0, 0, 0, -1 ]
-                              ])
-    
-    # A famous spacetime which describes time travel.
-    f = Rational('1/2') * w**(-2)
-    godel_spacetime = Matrix([
-                                  [ f, 0, f * 2*E**(2 * x1), 0 ], 
-                                  [ 0, -1, 0, 0 ], 
-                                  [ f * 2*E**(2 * x1), 0, -f*Rational('1/2')*E**x1, 0 ], 
-                                  [ 0, 0, 0, -f ]
-                              ])
-    
-    
-    # Rotataing uncharged black hole.
-    a, J, M, c, delt, sigm = symbols('a J M c Delta Sigma')
-    a = J / ( M * c )
-    rs = 2 * G * M / ( c**2 )
-    sigm = x1**2 + a**2 * cos(x2)**2
-    delt = (r**2 - x0 * rs + a**2)
-    kerr_spacetime = Matrix([
-                                [ (1 - rs * x1 / sigm ), 0, 0, 2 * G * M / ( c**2 ) ], 
-                                [ 0, -1 * ( delt / sigm ), 0, 0 ], 
-                                [ 0, 0, -1 * sigm, 0 ],
-                                [ 2 * G * M / ( c**2 ), 0, 0, -1 * (x1**2 + a**2 + (rs*x1*a**2/sigm)*sin(x2))*sin(x2) ]
-                            ])
-
-    #Ref: https://en.wikipedia.org/wiki/Taub%E2%80%93NUT_space
-    #Latex: 
-    U, m, l = symbols('U m l')
-    U = (2 * m * x0 + l**2 - x0**2) / ( x0**2 + l**2 )
-    taub_nut_spacetime = Matrix([
-                                    [ U**(-1), 0, 0, 0 ], 
-                                    [ 0, -4 * l**2 * U, -1 * cos(x2) * (4 * l**2 * U), 0 ], 
-                                    [ 0, -1 * cos(x2) * (4 * l**2 * U), -1 * (x0**2 + l**2), 0 ],
-                                    [ 0, 0, 0, -1 * (x0**2 + l**2) * sin(x2) - (4 * l**2 * U) * cos(x2)**2 ]
-                                ])
-    # 
-    ozsvath_schucking_spacetime = Matrix([
-                                             [ 1, 0, -2*x3, 0 ], 
-                                             [ 0, 0, 1*x3, 0 ], 
-                                             [ -2*x3, 1*x3, -1, 1 ], 
-                                             [ 0, 0, 1, 1 ]
-                                         ])
-    #
-    G, M, c, Q, k = symbols('G M c Q k')
-    rs = 2 * G * M / ( c**2 )
-    rq = Q**2 * G * k / c**4
-    reissner_nordstrom_spacetime = Matrix([
-                                              [ (1 + rs / x1 + rq**2 / x0**2), 0, 0, 0 ], 
-                                              [ 0, -1 * (1 + rs / x1 + rq**2 / x0**2)**(-1), 0, 0 ], 
-                                              [ 0, 0, -x1**2, 0 ], 
-                                              [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
-                                          ])
-    # Rotataing charged black hole.
-    G, M, c, Q, k = symbols('G M c Q k')
-    rs = 2 * G * M / ( c**2 )
-    rq = Q**2 * G * k / c**4
-    kerr_newman_spacetime = Matrix([
-                                       [ (1 + rs / x1 + rq**2 / x0**2), 0, 0, 0 ], 
-                                       [ 0, -1 * (1 + rs / x1 + rq**2 / x0**2)**(-1), 0, 0 ], 
-                                       [ 0, 0, -x1**2, 0 ], 
-                                       [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
-                                   ])
-    
-    
-    # The classic warp drive solution. (This takes a long time to process!!!) (I suspect the compute time diverges. I think there is some recursive error)
-    # Does not crash.
-    xs = symbols('x_s')(x0)
-    vs = xs.diff(x0)
-    rs = sqrt((x1-xs)**2 + x2**2 + x3**2)
-    fs = tanh(s * (rs + R)) - tanh(s * (rs - R)) / (2 * tanh( s * R ))
-    alcubierre_spacetime = Matrix([
-                                      [ (vs**2 * fs**2 - 1), -2*vs*fs, -2*vs*fs, -2*vs*fs ], 
-                                      [ -2*vs*fs, -1, 0, 0 ], 
-                                      [ -2*vs*fs, 0, -1, 0 ], 
-                                      [ -2*vs*fs, 0, 0, -1 ]
-                                  ])
-    
-    
-    # Rotataing uncharged black hole.
-    a, J, M, c, delt, sigm = symbols('a J M c Delta Sigma')
-    a = (J/(M*c))
-    rs = (2*G*M/(c**2))
-    sigm = (x1**2 + (J/(M*c))**2 * cos(x2)**2)
-    delt = (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2)
-    kerr_spacetime = Matrix([
-                                [ (1 - rs * x1 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0, (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ], 
-                                [ 0, -1 * ( (r**2 - x0 * (2*G*M/(c**2)) + (J/(M*c))**2) / (x1**2 + (J/(M*c))**2 * cos(x2)**2) ), 0, 0 ], 
-                                [ 0, 0, -1 * (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0 ],
-                                [ (2*G*M/(c**2))*x1*(J/(M*c))*sin(x2)**2 / (x1**2 + (J/(M*c))**2 * cos(x2)**2), 0, 0, -1 * (x1**2 + (J/(M*c))**2 + (rs*x1*(J/(M*c))**2/(x1**2 + (J/(M*c))**2 * cos(x2)**2))*sin(x2))*sin(x2) ]
-                            ])
-    
-    
-    """
-
-    
-    """
-    # Rotataing charged black hole.
-    G, M, c, Q, k = symbols('G M c Q k')
-    rs = 2 * G * M / ( c**2 )
-    rq = Q**2 * G * k / c**4
-    kerr_newman_spacetime = Matrix([
-                                       [ (1 + rs / x1 + rq**2 / x0**2), 0, 0, 0 ], 
-                                       [ 0, -1 * (1 + rs / x1 + rq**2 / x0**2)**(-1), 0, 0 ], 
-                                       [ 0, 0, -x1**2, 0 ], 
-                                       [ 0, 0, 0, -x1**2 * sin(x2)**2 ]
-                                   ])
-    
-    # The classic warp drive solution. (This takes a long time to process!!!) (I suspect the compute time diverges. I think there is some recursive error)
-    # Does not crash.
-    xs = symbols('x_s')(x0)
-    vs = xs.diff(x0)
-    rs = sqrt((x1-xs)**2 + x2**2 + x3**2)
-    fs = tanh(s * (rs + R)) - tanh(s * (rs - R)) / (2 * tanh( s * R ))
-    alcubierre_spacetime = Matrix([
-                                      [ (vs**2 * fs**2 - 1), -2*vs*fs, -2*vs*fs, -2*vs*fs ], 
-                                      [ -2*vs*fs, -1, 0, 0 ], 
-                                      [ -2*vs*fs, 0, -1, 0 ], 
-                                      [ -2*vs*fs, 0, 0, -1 ]
-                                  ])
-    """
-    coordinate_set = [ x0, x1, x2, x3 ]
-    spacetime = SpaceTime(schwarzschild_spacetime, coordinate_set)
-
-    #kerr_spacetime = Spacetime(.kerr_metric)
-    
-    #observer_1 = Frame(x,coordinate_set)
-    #observer_2 = Frame(initial_position,initial_velocity,coordinate_set)
-    #spacetime.set_metric_coefficient("dd",0,0,23)
-    #spacetime.set_metric_coefficient("uu",0,0,23)
-    
-    #spacetime.set_connection_coefficient("udd",0,0,0,23)
-    #spacetime.set_connection_coefficient("ddd",0,0,23)
-    
-    #spacetime.set_riemann_coefficient("uddd",0,0,23)
-    #spacetime.set_riemann_coefficient("dddd",0,0,23)
-    
-    #spacetime.set_ricci_coefficient("dd",0,0,23)
-    #spacetime.set_ricci_coefficient("uu",0,0,23)
-    
-    #spacetime.set_einstein_coefficient("dd",0,0,23)
-    #spacetime.set_einstein_coefficient("dd",0,0,23)
-    
-    #spacetime.set_stress_energy_coefficient("dd",0,0,23)
-    #spacetime.set_stress_energy_coefficient("dd",0,0,23)
-    
-    #pprint(spacetime.get_connection_coefficient("udd",0,1,1))
-    #pprint(spacetime.get_connection_coefficient("udd",0,2,2))
-    #pprint(spacetime.get_connection_coefficient("udd",0,3,3))
-    
-    print("")
-    print("")
-    print("Metric tensor coefficients (dd)")
-    print("================================")
-    spacetime.print_all_metric_coefficients("dd")
-    
-    print("")
-    print("")
-    print("Connection coefficients (udd)")
-    print("=============================")
-    spacetime.print_all_connection_coefficients("udd")
-    
-    #print("")
-    #print("")
-    #print("Connection coefficients (ddd)")
-    #print("=============================")
-    #spacetime.print_all_connection_coefficients("ddd")
-    
-    print("")
-    print("")
-    print("Riemann curvature tensor coefficients (uddd)")
-    print("============================================")
-    spacetime.print_all_riemann_coefficients("uddd")
-   
-    
-    print("")
-    print("")
-    print("Ricci curvature tensor coefficients (dd)")
-    print("========================================")
-    spacetime.print_all_ricci_coefficients("dd")
-    
-    print("")
-    print("")
-    print("Ricci curvature scalar")
-    print("======================")
-    spacetime.print_ricci_scalar()
-
-    
-    print("")
-    print("")
-    print("Einstein curvature tensor coefficients (dddd)")
-    print("=============================================")
-    spacetime.print_all_einstein_coefficients("dd")
-    
-    
-    print("")
-    print("")
-    print("Stress-energy-momentum tensor coefficients (dd)")
-    print("===============================================")
-    spacetime.print_all_stress_energy_coefficients("dd")
-    """
-    print("")
-    print("")
-    print("General geodesic equations parameterized by propter time.")
-    print("=========================================================")
-    spacetime.proper_time_geodesics(0,0,0)
-    
-    # Print out general geodesic equations.
-    # Solve for accelleration, velocity adn position vectors.
-    # Plot all vectors with respect to all relevent parameters.
-    """
     
 if __name__ == "__main__":
     main()
+
